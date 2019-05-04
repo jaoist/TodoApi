35f3cb314a665142b07ff180438bd5446f02fc63
Justin O'Brien <jaocode@gmail.com>
2019-05-04 13:41:21 -0400

    Added databse context to dependency injection container, registered database context using in memory database "TodoList"

---
+++ TodoApi/Models/TodoContex.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models
+{
+    public class TodoContex : DbContext
+    {
+        public TodoContex(DbContextOptions<TodoContex> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
